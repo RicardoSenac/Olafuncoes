970283ef785783b842a4896d49b7e07f1ed3016e
RICARDO NASCIMENTO DA SILVA <ricardo.nsilva12@senacsp.edu.br>
2026-05-15 20:48:45 -0300

    vistgoria

--- src/App.java
+++ src/App.java
@@ -1,9 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        floar comissao = calcularPercentual (100.00f, 20);
+        float comissao = calcularPercentual (100.00f, 20);
+        System.out.println(comissao);
 
-
-
+        float novoValor = Utils.calcularAcrescimo (100.00f, 100.00f);
+            System.out.println(novoValor);
+    
+            
     }
 }
