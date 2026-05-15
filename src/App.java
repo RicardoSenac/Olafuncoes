df9258a34bbe0fb7bd9bb16979c713a8f1ddcb87
RICARDO NASCIMENTO DA SILVA <ricardo.nsilva12@senacsp.edu.br>
2026-05-15 20:25:30 -0300

    Versão Inicial

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        floar comissao = calcularPercentual (100.00f, 20);
+
+
+
+    }
+}
---
+++ src/Utils.java
@@ -0,0 +1,11 @@
+public class Utils {
+
+    public static float calcularPercentual (float valor, float percentual){
+
+        float percentualCalculado = (percentual/100);
+        return valor * percentualCalculado;
+        
+
+    }
+
+}
